3510069a155b72629e75d7ed89cc393d55e13816
nick <150211508+n1cksway@users.noreply.github.com>
2024-01-30 23:51:36 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,34 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  button();
+  cardGenerator();
 };
+
+function button() {
+  document.querySelector(".btn").addEventListener("click", function() {
+    cardGenerator();
+  });
+}
+function cardGenerator() {
+  const suits = ["♣", "♠", "♥", "♦"];
+  const numbers = ["2", "4", "8", "10", "K", "Q", "J", "♞𓆗"];
+
+  let randomSuits = suits[Math.floor(Math.random() * suits.length)];
+  let randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
+
+  if (randomSuits === "♦" || randomSuits === "♥") {
+    document.querySelector(".top").style.color = "red";
+    document.querySelector(".bottom").style.color = "red";
+  } else {
+    document.querySelector(".top").style.color = "black";
+    document.querySelector(".bottom").style.color = "black";
+  }
+  document.querySelector(".top").innerHTML = randomSuits;
+  document.querySelector(".number").innerHTML = randomNumbers;
+  document.querySelector(".bottom").innerHTML = randomSuits;
+}
